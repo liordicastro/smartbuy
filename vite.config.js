fab612e0f7c67ece5e45ee037268d1a1a06c14e2
Lior DiCastro <liordicastro@h-MacBook-Air-sl.local>
2026-02-26 21:00:39 +0200

    Fix: Add package-lock and vite config for successful build

--- vite.config.js
+++ vite.config.js
@@ -3,12 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['esbuild']
-  },
   build: {
-    commonjsOptions: {
-      transformMixedEsModules: true
-    }
+    outDir: 'dist',
   }
 })
